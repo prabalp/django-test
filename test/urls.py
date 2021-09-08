70812c5dc44386615be87f37430bafed7ec22de8
prabalp <prabalpanda2008@gmail.com>
2021-09-08 11:56:44 +0530

    apps

--- test/urls.py
+++ test/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+# admin panal settings
+admin.site.site_header = ""
+admin.site.site_title = ""
+admin.site.index_title = ""
+
+
+urlpatterns = [path("admin/", admin.site.urls), path("", include("home.urls"))]
